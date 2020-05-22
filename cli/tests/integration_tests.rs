vmg-dev/deno

commit a293eb8895a779b159933909cf5aa9a6aa278c81

refactor: rewrite TS dependency analysis in Rust (#5029)
This commit completely overhauls how module analysis is 
performed in TS compiler by moving the logic to Rust.

In the current setup module analysis is performed using 
"ts.preProcessFile" API in a special TS compiler worker 
running on a separate thread.

"ts.preProcessFile" allowed us to build a lot of functionality
in CLI including X-TypeScript-Types header support 
and @deno-types directive support. Unfortunately at the 
same time complexity of the ops required to perform 
supporting tasks exploded and caused some hidden 
permission escapes.

This PR introduces "ModuleGraphLoader" which can parse
source and load recursively all dependent source files; as 
well as declaration files. All dependencies used in TS 
compiler and now fetched and collected upfront in Rust 
before spinning up TS compiler.

To achieve feature parity with existing APIs this commit 
includes a lot of changes:

* add "ModuleGraphLoader"
  - can fetch local and remote sources
  - parses source code using SWC and extracts imports, exports, file references, special 
     headers
  - this struct inherited all of the hidden complexity and cruft from TS version and requires 
     several follow up PRs
* rewrite cli/tsc.rs to perform module analysis upfront and send all required source code to 
  TS worker in one message
* remove op_resolve_modules and op_fetch_source_files from cli/ops/compiler.rs
* run TS worker on the same thread

cli/tests/integration_tests.rs
@@ -1353,7 +1353,7 @@ itest!(error_004_missing_module {
 });
 
 itest!(error_005_missing_dynamic_import {
-  args: "run --reload error_005_missing_dynamic_import.ts",
+  args: "run --reload --allow-read error_005_missing_dynamic_import.ts",
   exit_code: 1,
   output: "error_005_missing_dynamic_import.ts.out",
 });

cli/file_fetcher.rs
@@ -34,6 +34,7 @@ pub struct SourceFile {
   pub url: Url,
   pub filename: PathBuf,
   pub types_url: Option<Url>,
+  pub types_header: Option<String>,
   pub media_type: msg::MediaType,
   pub source_code: Vec<u8>,
 }
@@ -323,6 +324,7 @@ impl SourceFileFetcher {
       media_type,
       source_code,
       types_url,
+      types_header: None,
     })
   }
 
@@ -380,6 +382,7 @@ impl SourceFileFetcher {
       &fake_filepath,
       headers.get("content-type").map(|e| e.as_str()),
     );
+    let types_header = headers.get("x-typescript-types").map(|e| e.to_string());
     let types_url = match media_type {
       msg::MediaType::JavaScript | msg::MediaType::JSX => get_types_url(
         &module_url,
@@ -394,6 +397,7 @@ impl SourceFileFetcher {
       media_type,
       source_code,
       types_url,
+      types_header,
     }))
   }
 
@@ -502,6 +506,8 @@ impl SourceFileFetcher {
             headers.get("content-type").map(String::as_str),
           );
 
+          let types_header =
+            headers.get("x-typescript-types").map(String::to_string);
           let types_url = match media_type {
             msg::MediaType::JavaScript | msg::MediaType::JSX => get_types_url(
               &module_url,
@@ -517,6 +523,7 @@ impl SourceFileFetcher {
             media_type,
             source_code: source,
             types_url,
+            types_header,
           };
 
           Ok(source_file)

cli/global_state.rs
@@ -95,6 +95,7 @@ impl GlobalState {
     maybe_referrer: Option<ModuleSpecifier>,
     target_lib: TargetLib,
     permissions: Permissions,
+    is_dyn_import: bool,
   ) -> Result<CompiledModule, ErrBox> {
     let state1 = self.clone();
     let state2 = self.clone();
@@ -115,14 +116,20 @@ impl GlobalState {
       | msg::MediaType::JSX => {
         state1
           .ts_compiler
-          .compile(state1.clone(), &out, target_lib, permissions.clone())
+          .compile(state1.clone(), &out, target_lib, permissions, is_dyn_import)
           .await
       }
       msg::MediaType::JavaScript => {
         if state1.ts_compiler.compile_js {
           state2
             .ts_compiler
-            .compile(state1.clone(), &out, target_lib, permissions.clone())
+            .compile(
+              state1.clone(),
+              &out,
+              target_lib,
+              permissions,
+              is_dyn_import,
+            )
             .await
         } else {
           if let Some(types_url) = out.types_url.clone() {

cli/main.rs
@@ -42,6 +42,7 @@ pub mod installer;
 mod js;
 mod lockfile;
 mod metrics;
+mod module_graph;
 pub mod msg;
 pub mod op_error;
 pub mod ops;
@@ -69,6 +70,7 @@ pub use dprint_plugin_typescript::swc_ecma_parser;
 use crate::doc::parser::DocFileLoader;
 use crate::file_fetcher::SourceFile;
 use crate::file_fetcher::SourceFileFetcher;
+use crate::fs as deno_fs;
 use crate::global_state::GlobalState;
 use crate::msg::MediaType;
 use crate::op_error::OpError;
@@ -210,6 +212,7 @@ async fn print_file_info(
       None,
       TargetLib::Main,
       Permissions::allow_all(),
+      false,
     )
     .await?;
 
@@ -354,6 +357,7 @@ async fn eval_command(
     filename: main_module_url.to_file_path().unwrap(),
     url: main_module_url,
     types_url: None,
+    types_header: None,
     media_type: if as_typescript {
       MediaType::TypeScript
     } else {
@@ -382,12 +386,21 @@ async fn bundle_command(
   source_file: String,
   out_file: Option<PathBuf>,
 ) -> Result<(), ErrBox> {
-  let module_name = ModuleSpecifier::resolve_url_or_path(&source_file)?;
+  let mut module_name = ModuleSpecifier::resolve_url_or_path(&source_file)?;
+  let url = module_name.as_url();
+
+  // TODO(bartlomieju): fix this hack in ModuleSpecifier
+  if url.scheme() == "file" {
+    let a = deno_fs::normalize_path(&url.to_file_path().unwrap());
+    let u = Url::from_file_path(a).unwrap();
+    module_name = ModuleSpecifier::from(u)
+  }
+
   let global_state = GlobalState::new(flags)?;
   debug!(">>>>> bundle START");
   let bundle_result = global_state
     .ts_compiler
-    .bundle(global_state.clone(), module_name.to_string(), out_file)
+    .bundle(global_state.clone(), module_name, out_file)
     .await;
   debug!(">>>>> bundle END");
   bundle_result
@@ -530,6 +543,7 @@ async fn test_command(
     filename: test_file_url.to_file_path().unwrap(),
     url: test_file_url,
     types_url: None,
+    types_header: None,
     media_type: MediaType::TypeScript,
     source_code: test_file.clone().into_bytes(),
   };

cli/msg.rs
@@ -2,9 +2,11 @@
 
 // Warning! The values in this enum are duplicated in js/compiler.ts
 // Update carefully!
+use serde::Serialize;
+
 #[allow(non_camel_case_types)]
 #[repr(i8)]
-#[derive(Clone, Copy, PartialEq, Debug)]
+#[derive(Clone, Copy, PartialEq, Debug, Serialize)]
 pub enum MediaType {
   JavaScript = 0,
   JSX = 1,

cli/ops/compiler.rs
@@ -1,152 +1,13 @@
 // Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
-use super::dispatch_json::Deserialize;
-use super::dispatch_json::JsonOp;
-use super::dispatch_json::Value;
-use crate::futures::future::try_join_all;
-use crate::op_error::OpError;
 use crate::state::State;
 use deno_core::CoreIsolate;
-use deno_core::ModuleLoader;
-use deno_core::ModuleSpecifier;
-use deno_core::ZeroCopyBuf;
-use futures::future::FutureExt;
 
-pub fn init(i: &mut CoreIsolate, s: &State) {
-  i.register_op("op_resolve_modules", s.stateful_json_op(op_resolve_modules));
-  i.register_op(
-    "op_fetch_source_files",
-    s.stateful_json_op(op_fetch_source_files),
-  );
-  let custom_assets = std::collections::HashMap::new(); // TODO(ry) use None.
+pub fn init(i: &mut CoreIsolate, _s: &State) {
+  let custom_assets = std::collections::HashMap::new();
+  // TODO(ry) use None.
+  // TODO(bartlomieju): is this op even required?
   i.register_op(
     "op_fetch_asset",
     deno_typescript::op_fetch_asset(custom_assets),
   );
-}
-
-#[derive(Deserialize, Debug)]
-struct SpecifiersReferrerArgs {
-  specifiers: Vec<String>,
-  referrer: Option<String>,
-}
-
-fn op_resolve_modules(
-  state: &State,
-  args: Value,
-  _data: Option<ZeroCopyBuf>,
-) -> Result<JsonOp, OpError> {
-  let args: SpecifiersReferrerArgs = serde_json::from_value(args)?;
-  let (referrer, is_main) = if let Some(referrer) = args.referrer {
-    (referrer, false)
-  } else {
-    ("<unknown>".to_owned(), true)
-  };
-
-  let mut specifiers = vec![];
-
-  for specifier in &args.specifiers {
-    let specifier = state
-      .resolve(specifier, &referrer, is_main)
-      .map_err(OpError::from)?;
-    specifiers.push(specifier.as_str().to_owned());
-  }
-
-  Ok(JsonOp::Sync(json!(specifiers)))
-}
-
-fn op_fetch_source_files(
-  state: &State,
-  args: Value,
-  _data: Option<ZeroCopyBuf>,
-) -> Result<JsonOp, OpError> {
-  let args: SpecifiersReferrerArgs = serde_json::from_value(args)?;
-
-  let ref_specifier = if let Some(referrer) = args.referrer {
-    let specifier = ModuleSpecifier::resolve_url(&referrer)
-      .expect("Referrer is not a valid specifier");
-    Some(specifier)
-  } else {
-    None
-  };
-
-  let s = state.borrow();
-  let global_state = s.global_state.clone();
-  let permissions = s.permissions.clone();
-  let perms_ = permissions.clone();
-  drop(s);
-  let file_fetcher = global_state.file_fetcher.clone();
-  let specifiers = args.specifiers.clone();
-  let future = async move {
-    let file_futures: Vec<_> = specifiers
-      .into_iter()
-      .map(|specifier| {
-        let file_fetcher_ = file_fetcher.clone();
-        let ref_specifier_ = ref_specifier.clone();
-        let perms_ = perms_.clone();
-        async move {
-          let resolved_specifier = ModuleSpecifier::resolve_url(&specifier)
-            .expect("Invalid specifier");
-          // TODO(bartlomieju): duplicated from `state.rs::ModuleLoader::load` - deduplicate
-          // Verify that remote file doesn't try to statically import local file.
-          if let Some(referrer) = ref_specifier_.as_ref() {
-            let referrer_url = referrer.as_url();
-            match referrer_url.scheme() {
-              "http" | "https" => {
-                let specifier_url = resolved_specifier.as_url();
-                match specifier_url.scheme() {
-                  "http" | "https" => {},
-                  _ => {
-                    let e = OpError::permission_denied("Remote module are not allowed to statically import local modules. Use dynamic import instead.".to_string());
-                    return Err(e.into());
-                  }
-                }
-              },
-              _ => {}
-            }
-          }
-          file_fetcher_
-            .fetch_source_file(&resolved_specifier, ref_specifier_, perms_)
-            .await
-        }
-        .boxed_local()
-      })
-      .collect();
-
-    let files = try_join_all(file_futures).await.map_err(OpError::from)?;
-    // We want to get an array of futures that resolves to
-    let v = files.into_iter().map(|f| {
-      async {
-        // if the source file contains a `types_url` we need to replace
-        // the module with the type definition when requested by the compiler
-        let file = match f.types_url {
-          Some(types_url) => {
-            let types_specifier = ModuleSpecifier::from(types_url);
-            global_state
-              .file_fetcher
-              .fetch_source_file(
-                &types_specifier,
-                ref_specifier.clone(),
-                permissions.clone(),
-              )
-              .await
-              .map_err(OpError::from)?
-          }
-          _ => f,
-        };
-        let source_code = String::from_utf8(file.source_code).map_err(|_| OpError::invalid_utf8())?;
-        Ok::<_, OpError>(json!({
-          "url": file.url.to_string(),
-          "filename": file.filename.to_str().unwrap(),
-          "mediaType": file.media_type as i32,
-          "sourceCode": source_code,
-        }))
-      }
-    });
-
-    let v = try_join_all(v).await?;
-    Ok(v.into())
-  }
-  .boxed_local();
-
-  Ok(JsonOp::Async(future))
 }

commit 07bb4dd08216c6a19bca32f3f8fa78868cea91a3

fix: redirects handling in module analysis (#5726)
This commit fixes a bug introduced in #5029 that caused bad 
handling of redirects during module analysis. 

Also ensured that duplicate modules are not downloaded.

cli/js/compiler.ts
@@ -585,7 +585,6 @@ function buildSourceFileCache(
   sourceFileMap: Record<string, SourceFileMapEntry>
 ): void {
   for (const entry of Object.values(sourceFileMap)) {
-    assert(entry.sourceCode.length > 0);
     SourceFile.addToCache({
       url: entry.url,
       filename: entry.url,
@@ -596,7 +595,15 @@ function buildSourceFileCache(
     for (const importDesc of entry.imports) {
       let mappedUrl = importDesc.resolvedSpecifier;
       const importedFile = sourceFileMap[importDesc.resolvedSpecifier];
+      // IMPORTANT: due to HTTP redirects we might end up in situation
+      // where URL points to a file with completely different URL.
+      // In that case we take value of `redirect` field and cache
+      // resolved specifier pointing to the value of the redirect.
+      // It's not very elegant solution and should be rethinked.
       assert(importedFile);
+      if (importedFile.redirect) {
+        mappedUrl = importedFile.redirect;
+      }
       const isJsOrJsx =
         importedFile.mediaType === MediaType.JavaScript ||
         importedFile.mediaType === MediaType.JSX;
@@ -1032,6 +1039,7 @@ interface SourceFileMapEntry {
   url: string;
   sourceCode: string;
   mediaType: MediaType;
+  redirect?: string;
   imports: ImportDescriptor[];
   referencedFiles: ReferenceDescriptor[];
   libDirectives: ReferenceDescriptor[];

cli/module_graph.rs
@@ -19,6 +19,7 @@ use futures::FutureExt;
 use serde::Serialize;
 use serde::Serializer;
 use std::collections::HashMap;
+use std::collections::HashSet;
 use std::hash::BuildHasher;
 use std::path::PathBuf;
 use std::pin::Pin;
@@ -76,6 +77,7 @@ pub struct ReferenceDescriptor {
 pub struct ModuleGraphFile {
   pub specifier: String,
   pub url: String,
+  pub redirect: Option<String>,
   pub filename: String,
   pub imports: Vec<ImportDescriptor>,
   pub referenced_files: Vec<ReferenceDescriptor>,
@@ -87,13 +89,14 @@ pub struct ModuleGraphFile {
 }
 
 type SourceFileFuture =
-  Pin<Box<dyn Future<Output = Result<SourceFile, ErrBox>>>>;
+  Pin<Box<dyn Future<Output = Result<(ModuleSpecifier, SourceFile), ErrBox>>>>;
 
 pub struct ModuleGraphLoader {
   permissions: Permissions,
   file_fetcher: SourceFileFetcher,
   maybe_import_map: Option<ImportMap>,
   pending_downloads: FuturesUnordered<SourceFileFuture>,
+  has_downloaded: HashSet<ModuleSpecifier>,
   pub graph: ModuleGraph,
   is_dyn_import: bool,
   analyze_dynamic_imports: bool,
@@ -112,6 +115,7 @@ impl ModuleGraphLoader {
       permissions,
       maybe_import_map,
       pending_downloads: FuturesUnordered::new(),
+      has_downloaded: HashSet::new(),
       graph: ModuleGraph(HashMap::new()),
       is_dyn_import,
       analyze_dynamic_imports,
@@ -131,8 +135,9 @@ impl ModuleGraphLoader {
     self.download_module(specifier.clone(), None)?;
 
     loop {
-      let source_file = self.pending_downloads.next().await.unwrap()?;
-      self.visit_module(&source_file.url.clone().into(), source_file)?;
+      let (specifier, source_file) =
+        self.pending_downloads.next().await.unwrap()?;
+      self.visit_module(&specifier, source_file)?;
       if self.pending_downloads.is_empty() {
         break;
       }
@@ -260,6 +265,7 @@ impl ModuleGraphLoader {
       ModuleGraphFile {
         specifier: specifier.to_string(),
         url: specifier.to_string(),
+        redirect: None,
         media_type: map_file_extension(&PathBuf::from(specifier.clone()))
           as i32,
         filename: specifier,
@@ -281,7 +287,7 @@ impl ModuleGraphLoader {
     module_specifier: ModuleSpecifier,
     maybe_referrer: Option<ModuleSpecifier>,
   ) -> Result<(), ErrBox> {
-    if self.graph.0.contains_key(&module_specifier.to_string()) {
+    if self.has_downloaded.contains(&module_specifier) {
       return Ok(());
     }
 
@@ -319,6 +325,7 @@ impl ModuleGraphLoader {
       }
     }
 
+    self.has_downloaded.insert(module_specifier.clone());
     let spec = module_specifier;
     let file_fetcher = self.file_fetcher.clone();
     let perms = self.permissions.clone();
@@ -328,13 +335,7 @@ impl ModuleGraphLoader {
       let source_file = file_fetcher
         .fetch_source_file(&spec_, maybe_referrer, perms)
         .await?;
-      // FIXME(bartlomieju):
-      // because of redirects we may end up with wrong URL,
-      // substitute with original one
-      Ok(SourceFile {
-        url: spec_.as_url().to_owned(),
-        ..source_file
-      })
+      Ok((spec_.clone(), source_file))
     }
     .boxed_local();
 
@@ -353,6 +354,33 @@ impl ModuleGraphLoader {
     let mut types_directives = vec![];
     let mut type_headers = vec![];
 
+    // IMPORTANT: source_file.url might be different than requested
+    // module_specifier because of HTTP redirects. In such
+    // situation we add an "empty" ModuleGraphFile with 'redirect'
+    // field set that will be later used in TS worker when building
+    // map of available source file. It will perform substitution
+    // for proper URL point to redirect target.
+    if module_specifier.as_url() != &source_file.url {
+      // TODO(bartlomieju): refactor, this is a band-aid
+      self.graph.0.insert(
+        module_specifier.to_string(),
+        ModuleGraphFile {
+          specifier: module_specifier.to_string(),
+          url: module_specifier.to_string(),
+          redirect: Some(source_file.url.to_string()),
+          filename: source_file.filename.to_str().unwrap().to_string(),
+          media_type: source_file.media_type as i32,
+          source_code: "".to_string(),
+          imports: vec![],
+          referenced_files: vec![],
+          lib_directives: vec![],
+          types_directives: vec![],
+          type_headers: vec![],
+        },
+      );
+    }
+
+    let module_specifier = ModuleSpecifier::from(source_file.url.clone());
     let source_code = String::from_utf8(source_file.source_code)?;
 
     if source_file.media_type == MediaType::JavaScript
@@ -470,7 +498,8 @@ impl ModuleGraphLoader {
       module_specifier.to_string(),
       ModuleGraphFile {
         specifier: module_specifier.to_string(),
-        url: source_file.url.to_string(),
+        url: module_specifier.to_string(),
+        redirect: None,
         filename: source_file.filename.to_str().unwrap().to_string(),
         media_type: source_file.media_type as i32,
         source_code,

tools/http_server.py
@@ -122,6 +122,34 @@ class ContentTypeHandler(QuietSimpleHTTPRequestHandler):
             self.wfile.write(bytes("export const foo = 'foo';"))
             return
 
+        if "type_directives_redirect.js" in self.path:
+            self.protocol_version = "HTTP/1.1"
+            self.send_response(200, 'OK')
+            self.send_header('Content-type', 'application/javascript')
+            self.send_header(
+                'X-TypeScript-Types',
+                'http://localhost:4547/xTypeScriptTypesRedirect.d.ts')
+            self.end_headers()
+            self.wfile.write(bytes("export const foo = 'foo';"))
+            return
+
+        if "xTypeScriptTypesRedirect.d.ts" in self.path:
+            self.protocol_version = "HTTP/1.1"
+            self.send_response(200, 'OK')
+            self.send_header('Content-type', 'application/typescript')
+            self.end_headers()
+            self.wfile.write(
+                bytes("import './xTypeScriptTypesRedirected.d.ts';"))
+            return
+
+        if "xTypeScriptTypesRedirected.d.ts" in self.path:
+            self.protocol_version = "HTTP/1.1"
+            self.send_response(200, 'OK')
+            self.send_header('Content-type', 'application/typescript')
+            self.end_headers()
+            self.wfile.write(bytes("export const foo: 'foo';"))
+            return
+
         if "xTypeScriptTypes.d.ts" in self.path:
             self.protocol_version = "HTTP/1.1"
             self.send_response(200, 'OK')

cli/tests/integration_tests.rs
@@ -1565,6 +1565,12 @@ itest!(type_directives_js_main {
   exit_code: 0,
 });
 
+itest!(type_directives_redirect {
+  args: "run --reload type_directives_redirect.ts",
+  output: "type_directives_redirect.ts.out",
+  http_server: true,
+});
+
 itest!(types {
   args: "types",
   output: "types.out",

cli/tests/type_directives_redirect.ts
@@ -1,0 +1,1 @@
+import "http://localhost:4545/type_directives_redirect.js";

cli/tests/type_directives_redirect.ts.out
@@ -1,0 +1,5 @@
+Download [WILDCARD]type_directives_redirect.js
+Download [WILDCARD]xTypeScriptTypesRedirect.d.ts
+Download [WILDCARD]xTypeScriptTypesRedirect.d.ts
+Download [WILDCARD]xTypeScriptTypesRedirected.d.ts
+Compile [WILDCARD]type_directives_redirect.ts