2f75bed2044ed4b416e8f6b884b3927477ef4d3f
Kitson Kelly <me@kitsonkelly.com>
2022-06-01 10:19:18 +1000

    feat: update to TypeScript 4.7 (#14242)

--- cli/dts/lib.es2020.number.d.ts
+++ cli/dts/lib.es2020.number.d.ts
@@ -18,11 +18,13 @@ and limitations under the License.
 /// <reference no-default-lib="true"/>
 
 
-interface ObjectConstructor {
+/// <reference lib="es2020.intl" />
+
+interface Number {
     /**
-     * Determines whether an object has a property with the specified name.
-     * @param o An object.
-     * @param v A property name.
+     * Converts a number to a string by using the current or specified locale.
+     * @param locales A locale string, array of locale strings, Intl.Locale object, or array of Intl.Locale objects that contain one or more language or locale tags. If you include more than one locale string, list them in descending order of priority so that the first entry is the preferred locale. If you omit this parameter, the default locale of the JavaScript runtime is used.
+     * @param options An object that contains one or more properties that specify comparison options.
      */
-    hasOwn(o: object, v: PropertyKey): boolean;
+    toLocaleString(locales?: Intl.LocalesArgument, options?: Intl.NumberFormatOptions): string;
 }
